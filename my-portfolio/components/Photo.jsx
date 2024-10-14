9931a8b41560aa152b18c9c9e83198269f5fc189
Hina Samreen <hinasamreen541@gmail.com>
2024-10-14 18:17:49 +0200

    Home page von meinem Protfolio

---
+++ my-portfolio/components/Photo.jsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { motion } from "framer-motion";
+import Image from "next/image";
+
+const Photo = () => {
+  return (
+    <div className="w-full h-full relative">
+        <motion.div initial={{opacity:0}} animate={{ opacity:1, transition:{ delay:2, duration:0.4, ease:"easeIn"},}}>
+            <div className="w-[298px] h-[298px] xl:w-[400px] xl:h-[400px] mix-blend-lighten">
+                <Image src="/assets/MyPhoto.webp"
+                        priority quality={100} fill alt="" className="object-contain"></Image>
+            </div>
+        </motion.div>
+    </div>
+  )
+}
+
+export default Photo
